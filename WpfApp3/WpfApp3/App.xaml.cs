15833d64286aa96ae0cdfb3507f485f3892cd378
Andrey <andrey.venzhega@gmail.com>
2018-10-09 23:40:42 +0300

    init

---
+++ WpfApp3/WpfApp3/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Data.Entity;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static visitorsDBEntities _db;
+        public static visitorsDBEntities Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    _db = new visitorsDBEntities();
+                }
+                return _db;
+            }
+        }
+
+        public  DbSet<Visitor> Visitor { get => Db.Visitor; }
+        
+    }
+}
---
+++ WpfApp3/WpfApp3/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    class MainWindowViewModel
+    {
+        //List<Visitor> visitors;
+        public MainWindowViewModel()
+        {
+            WindowLogin windowLogin = new WindowLogin();
+            windowLogin.ShowDialog();
+        }
+
+        public List<Visitor> Visitors { get => App.Db.Visitor.ToList(); }
+    }
+}
