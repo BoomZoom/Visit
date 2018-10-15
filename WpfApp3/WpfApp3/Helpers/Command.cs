dc9acaa6bb45b04044a97a3a43f3f78ee47bd303
Andrey <andrey.venzhega@gmail.com>
2018-10-15 21:37:06 +0300

    change command

--- WpfApp3/WpfApp3/Helpers/Command.cs
+++ WpfApp3/WpfApp3/Helpers/Command.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
-namespace WpfApp3.Helpers
+namespace WpfApp3
 {
-    class Command
+    class Command : ICommand
     {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
+        public Command(Action<object> action)
+        {
+            this.execute = action;
+        }
+        public Action<object> Action { get => execute; private set => execute = value; }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute?.Invoke(parameter);
+        }
     }
 }
